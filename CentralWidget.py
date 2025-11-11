cee02da670ee4164dafa640a678d2ee6b633d65e
student <rene.bezold@sbs-herzogenaurach.de>
2025-11-11 10:56:30 +0100

    test

--- CentralWidget.py
+++ CentralWidget.py
@@ -1,38 +1,59 @@
 from PyQt6.QtCore import pyqtSlot
-from PyQt6.QtWidgets import QWidget, QPushButton, QLineEdit, QTextBrowser, QVBoxLayout, QGridLayout
+from PyQt6.QtWidgets import QWidget, QPushButton, QLineEdit, QTextBrowser, QVBoxLayout, QGridLayout, QLabel
 
 
 class CentralWidget(QWidget):
     def __init__(self, parent=None):
         super(CentralWidget, self).__init__(parent)
 
-        self.__push_button_append = QPushButton("Hinzufügen")
-        self.__push_button_append.clicked.connect(self.append)
+        self.__login = QPushButton("Login")
+        self.__logout = QPushButton("Logout")
 
-        self.__push_button_text = QPushButton("set_text")
-        self.__push_button_text.clicked.connect(self.set_text)
+        self.__benutzername = QLabel("Benutzername")
+        self.__passwort = QLabel("Passwort")
+        self.__token = QLabel("Token")
 
-        self.__push_button_clear = QPushButton("Text löschen")
-        self.__push_button_clear.clicked.connect(self.clear)
 
-        self.__push_button_backward = QPushButton("Zurück")
-        self.__push_button_backward.clicked.connect(self.backward)
 
         self.__line_edit = QLineEdit()
+        self.__line_edit.setPlaceholderText("max_power")
+
+
+        self.__line_edit_pw = QLineEdit()
+
+        self.__line_edit_token = QLineEdit()
 
         self.__text_browser = QTextBrowser()
-        self.__text_browser.setUndoRedoEnabled(True)
+
+
 
         layout = QGridLayout()
-        layout.addWidget(self.__line_edit, 0, 0, 1, 2)
-        layout.addWidget(self.__push_button_append, 1, 0)
-        layout.addWidget(self.__push_button_text, 1, 1)
-        layout.addWidget(self.__push_button_clear, 2, 0)
-        layout.addWidget(self.__push_button_backward, 2, 1)
-        layout.addWidget(self.__text_browser, 3, 0, 1, 2)
+        layout.addWidget(self.__benutzername, 0, 0)
+        layout.addWidget(self.__line_edit, 0, 1, 1, 10)
+        layout.addWidget(self.__passwort, 1, 0)
+        layout.addWidget(self.__line_edit_pw, 1, 1, 1, 10)
+        layout.addWidget(self.__token, 2, 0)
+        layout.addWidget(self.__line_edit_token, 2, 1, 1, 10)
+        layout.addWidget(self.__login, 3, 0)
+        layout.addWidget(self.__logout, 3, 1, 1, 10)
+
+        # Ergebnisfeld
+        layout.addWidget(self.__text_browser, 4, 0, 6, 11)
+
 
         self.setLayout(layout)
 
+
+
+
+
+
+
+
+
+
+    '''
+    
     @pyqtSlot()
     def append(self):
         text = self.__line_edit.text()
@@ -52,3 +73,6 @@ class CentralWidget(QWidget):
     @pyqtSlot()
     def backward(self):
         self.__text_browser.undo()
+
+    
+    '''
